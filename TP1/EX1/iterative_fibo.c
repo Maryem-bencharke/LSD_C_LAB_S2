2421bd56925b289bfc815ee62ea5334308833f70
Maryem-ben-charke <72795298+Maryem-bencharke@users.noreply.github.com>
2021-03-22 10:43:52 +0100

    Update iterative_fibo.c

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -2,5 +2,14 @@
 
 int iterative_fibo(int n)
 {
-    // write your iterative_fibo code here
+    int i;
+	int f0=0,f1=1,f=0;
+	if(n==0 || n==1)
+		return n;
+	for(i=2;i<=n;i++){
+		f=f0 + f1;
+		f0=f1;
+		f1=f;
+	}
+	return f;
 }
