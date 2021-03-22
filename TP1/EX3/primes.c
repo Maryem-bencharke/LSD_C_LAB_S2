9f7f896a5eb17fbf84ce8e8543be9d4bc8c28758
Maryem-ben-charke <72795298+Maryem-bencharke@users.noreply.github.com>
2021-03-22 21:21:18 +0100

    Update primes.c

--- TP1/EX3/primes.c
+++ TP1/EX3/primes.c
@@ -8,12 +8,31 @@
 
 static bool    is_prime(int n)
 {
-    // write is_prime function code here
+    if (n==1)
+		return 0;
+	if  (n==0 || n==2)
+		return 0;
+	else
+	{ 
+		int i;
+		for(i=2;i<n;i++){
+			if (n%i==0)
+				return 0;
+			else
+				return 1;
+		}
+	}
+	return 0;
 }
 
 
 void    get_primes(int n)
 {
-    // write get_primes function code here that use is_prime function
+   int i;
+	for(i=2;i<=n;i++){
+		if (isprime(i)==1)
+			printf("%d is prime\n",i);
+	}
+	return 0;
 }
 
